771d41ad4d42c33097ebdf79fdc26e2378671208
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-08-20 18:40:05 -0400

    Add code to handle calculator input

--- index.js
+++ index.js
@@ -18,4 +18,104 @@ const operate = function (operator, n1, n2) {
   }
 };
 
+const buttons = document.querySelectorAll(".btn");
+const numberElem = document.querySelector(".display > p");
+
 let firstNumber, secondNumber, operator;
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    const operators = ["+", "-", "*", "/"];
+
+    switch (item.textContent) {
+      case "C":
+        clearCalculator();
+        break;
+      case "+/-":
+        changeSign();
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        setOperator(item.textContent);
+        break;
+      case "=":
+        doCalculation();
+        break;
+      default:
+        updateDisplay(item.textContent);
+        break;
+    }
+  };
+});
+
+const clearCalculator = function () {
+  numberElem.textContent = "";
+  firstNumber = undefined;
+  secondNumber = undefined;
+  operator = undefined;
+};
+
+const changeSign = function () {
+  const parsedNumber = parseFloat(numberElem.textContent);
+
+  if (!isNaN(parsedNumber) && parsedNumber !== 0) {
+    numberElem.textContent = parsedNumber * -1;
+  }
+};
+
+const updateDisplay = function (n) {
+  if (n === "." && numberElem.textContent.includes(".")) {
+    return;
+  }
+
+  if (numberElem.textContent === "0" && n !== ".") {
+    numberElem.textContent = "";
+  }
+  if (
+    firstNumber &&
+    !operator &&
+    parseFloat(numberElem.textContent) === firstNumber
+  ) {
+    numberElem.textContent = "";
+    firstNumber = undefined;
+  }
+
+  if (n === "." && numberElem.textContent === "") {
+    numberElem.textContent += "0.";
+  } else {
+    numberElem.textContent += n;
+  }
+};
+
+const setOperator = function (op) {
+  if (numberElem.textContent === "") {
+    return;
+  }
+
+  if (operator && firstNumber) {
+    doCalculation();
+  }
+
+  firstNumber = firstNumber || parseFloat(numberElem.textContent);
+  operator = op;
+  numberElem.textContent = "";
+};
+
+const doCalculation = function () {
+  if (!firstNumber || !operator || numberElem.textContent === "") {
+    return;
+  }
+
+  // get second number
+  secondNumber = parseFloat(numberElem.textContent);
+
+  const result = operate(operator, firstNumber, secondNumber);
+  const resultRounded = Math.round(result * 10000) / 10000; // round to 4 decimals
+
+  numberElem.textContent = resultRounded;
+  firstNumber = resultRounded;
+  secondNumber = undefined;
+  operator = undefined;
+};
